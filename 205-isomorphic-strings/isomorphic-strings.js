58b2789ce0f2a8fd96a8f1bf46d6de640baba0a6
Rajesh Behera <162548900+rajeshbehera16@users.noreply.github.com>
2025-07-09 21:09:09 +0530

    Time: 4 ms (88.85%) | Memory: 55.4 MB (40.22%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,20 @@
+var isIsomorphic = function(s, t) {
+    const charIndexS = {};
+    const charIndexT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (!(s[i] in charIndexS)) {
+            charIndexS[s[i]] = i;
+        }
+
+        if (!(t[i] in charIndexT)) {
+            charIndexT[t[i]] = i;
+        }
+
+        if (charIndexS[s[i]] !== charIndexT[t[i]]) {
+            return false;
+        }
+    }
+
+    return true;    
+};
