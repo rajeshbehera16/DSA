3699f0c6dafc427f52a7eb1ea729531e025fc8f3
Rajesh Behera <162548900+rajeshbehera16@users.noreply.github.com>
2025-07-10 11:50:42 +0530

    Time: 2 ms (74.66%) | Memory: 54 MB (94.92%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,25 @@
+// Valid Parentheses
+var isValid = function (s) {
+    let stack = [];
+    for(let i=0;i<s.length;i++){
+        if(s[i] === '(' || s[i]==='{'||s[i]==='['){
+            stack.push(s[i]);
+        }else{
+            const lastElement = stack.pop();
+            switch(lastElement){
+                case '(':
+                    if(s[i] !== ')') return false;
+                    break;
+                case '{':
+                    if(s[i] !== '}') return false;
+                    break;
+                case '[':
+                    if(s[i] !== ']') return false;
+                    break;
+                default:
+                    return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
