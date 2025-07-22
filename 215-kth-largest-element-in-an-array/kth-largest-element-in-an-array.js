4b04cff9b435a2914abca159791c68d3b1ed2964
Rajesh Behera <162548900+rajeshbehera16@users.noreply.github.com>
2025-07-22 12:27:35 +0530

    Time: 9 ms (95.49%) | Memory: 65.8 MB (68.41%) - LeetSync

---
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    const minValue = Math.min(...nums);
+    const maxValue = Math.max(...nums);
+
+    const count = new Array(maxValue - minValue + 1).fill(0);
+
+    for (const num of nums) {
+        count[num - minValue]++;
+    }
+
+    let remaining = k;
+    for (let i = count.length - 1; i >= 0; i--) {
+        remaining -= count[i];
+
+        if (remaining <= 0) {
+            return i + minValue;
+        }
+    }
+};
